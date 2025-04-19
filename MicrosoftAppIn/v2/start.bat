9f87f47cebad51f755ed02eae453996afdab1aad
lpooltestee <lpooltestee@gmail.com>
2025-04-19 13:25:48 -0300

    Delete MicrosoftAppIn/v2 directory

--- MicrosoftAppIn/v2/start.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d %~dp0
-call .venv\Scripts\activate
-
-:: Executa uvicorn com as configurações internas do main.py (.env já carregado lá)
-uvicorn backend.main:app --host 0.0.0.0 --port 8010 
